af106b82a14eccb8413736d07b64537efa52af89
Tony Young <ty_training@hotmail.co.uk>
2015-12-24 22:22:03 +0000

    Added SRP to top_up and changed in_journey to in_use

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,11 +5,11 @@ attr_reader :balance
 
   def initialize
     @balance = 0
-    @in_journey = false
+    @in_use = false
   end
 
   def top_up(amount)
-    fail "You have reached your top up limit of £#{MAX_BALANCE}" if @balance + amount > MAX_BALANCE
+    fail "You have reached your top up limit of £#{MAX_BALANCE}" if card_maxed_out?(amount)
     @balance += amount
   end
 
@@ -18,14 +18,20 @@ attr_reader :balance
   end
 
   def in_journey?
-    @in_journey
+    @in_use
   end
-
+  
   def tap_in
-    @in_journey = true
+    @in_use = true
   end
 
   def tap_out
-    @in_journey = false
+    @in_use = false
+  end
+
+  private
+
+  def card_maxed_out?(amount)
+    @balance + amount > MAX_BALANCE
   end
 end
